be3f3fa4269b38305d1712c8aca3c1670e17a22d
Animes <animesbarman101@gmail.com>
2025-05-31 18:17:56 +0600

    add key words

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('hello')
+  //=== javascript out
+
+// alert('hello javascript')
+// document.write('hello javascript');
+// console.log('<p>hello javascript</p>');
+
+//==keywords in js
+
+//break,case,catch,continue,debugger,dafault,delete,do,else,finally,for,function,if,in instanceof,new,return,switch,this,throw,try,typeof,var,void,while,with
+
