0c46a20fc82206509f116829a7ad0c4c115c68fa
Animes <animesbarman101@gmail.com>
2025-05-31 19:09:45 +0600

    add call 10 slice

--- script.js
+++ script.js
@@ -29,3 +29,81 @@
 
 // var a_$='use this case';
 // console.log(a_$);
+
+// var callName='cse';
+// console.log(callName);
+
+//==5 number method
+
+// var num=5000;
+// console.log(num);
+
+// var num=toString(num);
+// console.log(typeof(num))
+
+// var init='hello world';
+// init=parseInt(init);
+// init=parseFloat(init)
+// console.log(typeof(init));
+
+
+// var numberFixed=5000.9988;
+
+// console.log(numberFixed.toFixed(3));
+
+// console.log(numberFixed.toPrecision(5))
+
+// console.log(typeof(Number('animes')))
+
+// console.log(Number(true))
+// console.log(Number(false));
+
+
+//==9 contatiantion
+
+// var myName='Animes Barman';
+// var fatherName='Purna Chandra Barman';
+
+
+// var full=myName + ' '+ fatherName;
+
+// console.log(full);
+
+//==10  libary function for string
+
+// var jatioSongit='Amar sonar bangla ami tomay valo bashi';
+
+// console.log('number of charactor'+ ' '+ jatioSongit.length);
+
+
+// var name=prompt('enter your name');
+
+
+// document.write(name.length);
+
+// var namecharAt='Animes Barman';
+
+// console.log(namecharAt.charAt(5));
+
+//==Uppercase and Lower case
+
+// let text = "Hello World!";
+// let result = text.toUpperCase();
+
+// console.log(result);
+
+
+// let a='AIMES BARMAN0 '
+
+// let result2=a.toLowerCase();
+// console.log(result2);
+
+// var hello='he';
+// var hi='hello'
+
+// var out=hello.concat(hi);
+// console.log(out);
+
+var name1='Animes Barman is very good';
+var result = name1.slice(0,5);
+console.log(result);
