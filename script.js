771e23948134f8fd9efa67e3ed47d9896755bb49
Animes <animesbarman101@gmail.com>
2025-05-31 18:28:13 +0600

    add variable

--- script.js
+++ script.js
@@ -8,3 +8,24 @@
 
 //break,case,catch,continue,debugger,dafault,delete,do,else,finally,for,function,if,in instanceof,new,return,switch,this,throw,try,typeof,var,void,while,with
 
+//String
+// console.log('hello string');
+// nuber
+// console.log(40.40);
+// Boolean
+// console.log(typeof(true));
+
+
+//==5 variable
+
+// var name='Animes Barman';
+// console.log(name);
+// var mango=50;
+// var lichi=100;
+
+// var total=mango+lichi;
+
+// console.log(total);
+
+// var a_$='use this case';
+// console.log(a_$);
